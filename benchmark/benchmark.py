5297828f9d81d79fbee9ce59ac5cdfd2aa2c8f70
WiZ <yfbenkhalifa@gmail.com>
2025-02-24 18:26:21 +0100

    ✅ Adjusted Benchmark configuration + benchmark_checklist.csv upload

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,4 +1,7 @@
 import argparse
+
+from sympy import false
+
 import wandb
 import pandas as pd
 import os
@@ -12,9 +15,10 @@ from moviad.entrypoints.patchcore import PatchCoreArgs, train_patchcore
 from moviad.entrypoints.stfpm import STFPMArgs, train_stfpm
 
 seed = 42
+columns = ["Method", "Dataset type", "Class name", "Backbone", "AD layers",
+               "Contamination", "Runs", "State", "Error"]
 
-
-def update_dataframe(df, run):
+def update_dataframe(df, run, state="Running", error=""):
     existing_row = df[(df["Method"] == run.model) &
                       (df["Dataset type"] == run.dataset_type) &
                       (df["Class name"] == run.class_name) &
@@ -24,6 +28,7 @@ def update_dataframe(df, run):
 
     if not existing_row.empty:
         df.loc[existing_row.index, "Runs"] += 1
+        df.loc[existing_row.index, "State"] = state
     else:
         new_row = pd.DataFrame([{
             "Method": run.model,
@@ -32,9 +37,41 @@ def update_dataframe(df, run):
             "Backbone": run.backbone,
             "AD layers": run.ad_layers,
             "Contamination": run.contamination,
-            "Runs": 1
+            "Runs": 1,
+            "State": state,
+            "Error": error
         }])
         df = pd.concat([df, new_row], ignore_index=True)
+
+    return df
+
+
+def generate_full_checklist(df, benchmark_config: BenchmarkConfig):
+    new_runs = []
+    for benchmark_run in benchmark_config.get_benchmark_runs():
+        for run in benchmark_run.get_runs():
+            if run_exists(df, run): # Skip if run already exists
+                continue
+
+            run_data = {
+                "Method": run.model,
+                "Dataset type": run.dataset_type,
+                "Class name": run.class_name,
+                "Backbone": run.backbone,
+                "AD layers": str(run.ad_layers),
+                "Contamination": float(run.contamination),
+                "Runs": 0,
+                "State": "To run",
+                "Error": ""
+            }
+
+            new_runs.append(run_data)
+
+    # Create DataFrame with new runs
+    if new_runs:
+        new_df = pd.DataFrame(new_runs, columns=columns)
+        # Concatenate with existing DataFrame
+        df = pd.concat([df, new_df], ignore_index=True)
 
     return df
 
@@ -42,13 +79,15 @@ def update_dataframe(df, run):
 def save_dataframe(df, csv_file):
     df.to_csv(csv_file, index=False)
 
+
 def run_exists(df, run):
     existing_row = df[(df["Method"] == run.model) &
                       (df["Dataset type"] == run.dataset_type) &
                       (df["Class name"] == run.class_name) &
                       (df["Backbone"] == run.backbone) &
                       (df["AD layers"] == str(run.ad_layers)) &
-                      (df["Contamination"] == run.contamination)]
+                      (df["Contamination"] == run.contamination) &
+                      (df["State"] == "Completed")]
     return not existing_row.empty
 
 
@@ -130,36 +169,17 @@ def benchmark_stfpm(args: STFPMArgs, df, csv_file):
 
 
 class BenchmarkArgs:
-    def __init__(self, config_file, mode, model, dataset, category, backbone, ad_layers, epochs, save_path,
-                 visual_test_path, device, seed):
+    def __init__(self, config_file, mode,checklist_path):
         self.config_file = config_file
         self.mode = mode
-        self.model = model
-        self.dataset = dataset
-        self.category = category
-        self.backbone = backbone
-        self.ad_layers = ad_layers
-        self.epochs = epochs
-        self.save_path = save_path
-        self.visual_test_path = visual_test_path
-        self.device = device
-        self.seed = seed
+        self.checklist_path = checklist_path
 
     @classmethod
     def from_parser(cls, args):
         return cls(
             config_file=args.config_file,
             mode=args.mode,
-            model=args.model,
-            dataset=args.dataset,
-            category=args.category,
-            backbone=args.backbone,
-            ad_layers=args.ad_layers,
-            epochs=args.epochs,
-            save_path=args.save_path,
-            visual_test_path=args.visual_test_path,
-            device=args.device,
-            seed=args.seed
+            checklist_path=args.checklist_path
         )
 
 
@@ -167,19 +187,23 @@ def main(benchmark_args: BenchmarkArgs):
     # Parse the config file into DatasetConfig and BenchmarkConfig
     dataset_config = DatasetConfig(benchmark_args.config_file)
     benchmark_config = BenchmarkConfig(benchmark_args.config_file)
+    csv_file = "benchmark_checklist.csv" if benchmark_args.checklist_path is None else benchmark_args.checklist_path
 
-    # Initialize the DataFrame
-    columns = ["Method", "Dataset type", "Class name", "Backbone", "AD layers", "Contamination", "Runs"]
-    csv_file = "benchmark_checklist.csv"
     if os.path.exists(csv_file):
         df = pd.read_csv(csv_file)
     else:
         df = pd.DataFrame(columns=columns)
 
+    if benchmark_args.mode == "generate-checklist":
+        df = generate_full_checklist(df, benchmark_config)
+        save_dataframe(df, "benchmark_checklist.csv")
+        return
+
     for benchmark_run in benchmark_config.get_benchmark_runs():
         for run in benchmark_run.get_runs():
             if run_exists(df, run):
-                print(f"Run already exists: {run.model}, {run.dataset_type}, {run.class_name}, {run.backbone}, {run.ad_layers}, {run.contamination}")
+                print(
+                    f"Run already exists: {run.model}, {run.dataset_type}, {run.class_name}, {run.backbone}, {run.ad_layers}, {run.contamination}")
                 continue
             print(f"Method: {run.model}")
             print(f"Dataset type: {run.dataset_type}")
@@ -188,6 +212,9 @@ def main(benchmark_args: BenchmarkArgs):
             print(f"AD layers: {run.ad_layers}")
             print(f"Contamination: {run.contamination}")
             print("-------------------------------------")
+            state = "Running"
+            error = ""
+            df = update_dataframe(df, run, state=state, error=error)
             try:
                 if run.model == 'cfa':
                     args = CFAArguments(
@@ -199,13 +226,10 @@ def main(benchmark_args: BenchmarkArgs):
                         contamination_ratio=run.contamination,
                         seed=seed
                     )
-                    try:
-                        benchmark_cfa(args, df, csv_file)
-                        df = update_dataframe(df, run)
-                        save_dataframe(df, csv_file)
-                    except DatasetTooSmallToContaminateException:
-                        print(f"Dataset {run.dataset_type} is too small to contaminate. Skipping...")
-                        continue
+
+                    benchmark_cfa(args, df, csv_file)
+                    state = "Completed"
+
                 elif run.model == 'padim':
                     args = PadimArgs(
                         dataset_config=dataset_config,
@@ -216,13 +240,8 @@ def main(benchmark_args: BenchmarkArgs):
                         contamination_ratio=run.contamination,
                         seed=seed,
                     )
-                    try:
-                        benchmark_padim(args, df, csv_file)
-                        df = update_dataframe(df, run)
-                        save_dataframe(df, csv_file)
-                    except DatasetTooSmallToContaminateException:
-                        print(f"Dataset {run.dataset_type} is too small to contaminate. Skipping...")
-                        continue
+                    benchmark_padim(args, df, csv_file)
+                    state = "Completed"
                 elif run.model == 'patchcore':
                     args = PatchCoreArgs(
                         dataset_config=dataset_config,
@@ -234,13 +253,9 @@ def main(benchmark_args: BenchmarkArgs):
                         contamination_ratio=run.contamination,
                         seed=seed,
                     )
-                    try:
-                        benchmark_patchcore(args, df, csv_file)
-                        df = update_dataframe(df, run)
-                        save_dataframe(df, csv_file)
-                    except DatasetTooSmallToContaminateException:
-                        print(f"Dataset {run.dataset_type} is too small to contaminate. Skipping...")
-                        continue
+
+                    benchmark_patchcore(args, df, csv_file)
+                    state = "Completed"
                 elif run.model == 'patchcore_quantized':
                     args = PatchCoreArgs(
                         dataset_config=dataset_config,
@@ -253,13 +268,9 @@ def main(benchmark_args: BenchmarkArgs):
                         seed=seed,
                         quantized=True
                     )
-                    try:
-                        benchmark_patchcore(args, df, csv_file)
-                        df = update_dataframe(df, run)
-                        save_dataframe(df, csv_file)
-                    except DatasetTooSmallToContaminateException:
-                        print(f"Dataset {run.dataset_type} is too small to contaminate. Skipping...")
-                        continue
+                    benchmark_patchcore(args, df, csv_file)
+                    state = "Completed"
+
                 elif run.model == 'stfpm':
                     args = STFPMArgs(
                         dataset_config=dataset_config,
@@ -270,37 +281,47 @@ def main(benchmark_args: BenchmarkArgs):
                         contamination_ratio=run.contamination,
                         seed=seed
                     )
-                    try:
-                        benchmark_stfpm(args, df, csv_file)
-                        df = update_dataframe(df, run)
-                        save_dataframe(df, csv_file)
-                    except DatasetTooSmallToContaminateException:
-                        print(f"Dataset {run.dataset_type} is too small to contaminate. Skipping...")
-                        continue
+
+                    benchmark_stfpm(args, df, csv_file)
+                    state = "Completed"
+
+            except DatasetTooSmallToContaminateException:
+                print(f"Dataset {run.dataset_type} is too small to contaminate. Skipping...")
+                state = "Failed"
+                error = "Dataset too small to contaminate"
+                update_dataframe(df, run, state=state, error=error)
+                save_dataframe(df, csv_file)
+                continue
             except Exception as e:
                 print(f"An error occurred: {e}")
+                state = "Failed"
+                error = str(e)
+                update_dataframe(df, run, state=state, error=error)
+                save_dataframe(df, csv_file)
                 continue
 
+            update_dataframe(df, run, state=state, error=error)
+            save_dataframe(df, csv_file)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--config-file", type=str, help="Path to the config file (specify only this argument)")
-    parser.add_argument("--csv-file", type=str, help="Path to the checklist file")
-    parser.add_argument("--mode", choices=["train", "test"], help="Script execution mode: train or test")
-    parser.add_argument("--model", choices=["cfa", "padim", "patchcore", "stfpm"],
-                        help="Script execution mode: train or test")
-    parser.add_argument("--dataset", choices=["mvtec", "realiad", "visa"], type=str, help="Dataset type")
-    parser.add_argument("--category", type=str, help="Dataset category to test")
-    parser.add_argument("--backbone", type=str, help="Model backbone")
-    parser.add_argument("--ad_layers", type=str, nargs="+", help="List of ad layers")
-    parser.add_argument("--epochs", type=int, help="Number of epochs")
-    parser.add_argument("--save_path", type=str, default=None, help="Path of the .pt file where to save the model")
-    parser.add_argument("--visual_test_path", type=str, default=None,
-                        help="Path of the directory where to save the visual paths")
-    parser.add_argument("--device", type=str, help="Where to run the script")
-    parser.add_argument("--seed", type=int, default=1, help="Execution seed")
+    parser.add_argument("--config-file",
+                        type=str,
+                        help="Path to the config file",
+                        required=True)
+
+    parser.add_argument("--mode",
+                        choices=["generate-checklist", "run"],
+                        help="Script execution mode: generate-checklist or run",
+                        default="run")
+
+    parser.add_argument("--checklist-path",  # Changed to optional argument with --
+                        type=str,
+                        default=None,
+                        help="Path of the checklist file")
+
     args = parser.parse_args()
     benchmark_args = BenchmarkArgs.from_parser(args)
     main(benchmark_args)
